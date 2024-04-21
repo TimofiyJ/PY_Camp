a4a2ad9dbe9cc818deb6043f159b9bbdcead2d95
kvernx <veronika.khomyn17@gmail.com>
2024-04-21 15:34:31 +0300

    sync arrival filter and button

--- frontend/src/components/HouseCatalog/HouseCatalog.jsx
+++ frontend/src/components/HouseCatalog/HouseCatalog.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect, useRef } from "react";
 import { HouseBlock } from "../HouseBlock/HouseBlock";
 import "./HouseCatalog.css";
+import React, { useState, useEffect } from "react";
+import { ArrivalDropdown } from '../Filters/ArrivalFilter';
+
 
 export const HouseCatalogue = () => {
     const [data, setData] = useState([{}]);
-
     useEffect(() => {
         fetch("http://localhost:5000/houses?arrival=1")
             .then((res) => res.json())
@@ -14,22 +15,16 @@ export const HouseCatalogue = () => {
             });
     }, []);
 
-    // Функція, яка буде викликатися при кліці на кнопку
-    const handleButtonClick = () => {
-        // Ваш код для обробки кліку на кнопку тут
-        console.log("Кнопка була натиснута");
-    };
-
     return (
-        <div>
-            {/* Кнопка у правому верхньому кутку */}
-            <button className="button-right-top" onClick={handleButtonClick}>
-                Натисни мене
-            </button>
+        <div className="houses-container">
+            <div className="button-container">
+                <button className="button-right-top">Поселити дітей</button>
+                <ArrivalDropdown />
+            </div>
 
             <div className="houses-grid">
                 {data.map((house, idx) => (
-                    <HouseBlock key={idx} data={house} />
+                    <HouseBlock key={idx} data={house}/>
                 ))}
             </div>
         </div>
