c0fae9e91aaf27efa0d29b3005c8307ec271d8db
kvernx <veronika.khomyn17@gmail.com>
2024-04-28 22:23:12 +0300

    sync from main

---
+++ backend/models.py
@@ -0,0 +1,15 @@
+class Child:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+
+class House:
+    def __init__(self, capacity, name):
+        self.capacity = capacity
+        self.name = name
+        self.children = []
+
+    def __str__(self):
+        return str(str(self.capacity) + " " + str(self.name))
