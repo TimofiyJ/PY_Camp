442946888999ae6f2a198ba832370d4499741304
Tymofii Nasobko <44300490+TimofiyJ@users.noreply.github.com>
2024-04-30 22:34:13 +0300

    feat: sync data

--- database/load_data.py
+++ database/load_data.py
@@ -2,6 +2,30 @@ import random
 import pandas as pd
 from datetime import datetime, timedelta
 
+def generate_random_kid_date():
+    start_date = datetime(2007, 1, 1)
+    end_date = datetime(2016, 12, 31)
+    
+    # Calculate the difference in days between start_date and end_date
+    delta = end_date - start_date
+    
+    # Generate a random number of days within the range
+    random_days = random.randint(0, delta.days)
+    
+    # Add the random number of days to the start_date
+    random_date = start_date + timedelta(days=random_days)
+    
+    return random_date
+
+def generate_random_phone_number():
+    # Generate 9 random digits for the phone number
+    random_digits = ''.join([str(random.randint(0, 9)) for _ in range(9)])
+    
+    # Concatenate with the country code (380 for Ukraine)
+    phone_number = '380' + random_digits
+    
+    return phone_number
+
 # GENERATE HOUSE
 house_id = [i for i in range(1, 10)]
 house_name = [
@@ -17,7 +41,7 @@ house_name = [
 ]
 data = {"id":house_id,"name":house_name}
 houses = pd.DataFrame(data=data,columns=["id", "name"])
-houses.to_csv("./data/houses.csv", index=False)
+houses.to_csv("./data/House.csv", index=False)
 
 # GENERATE ROOM
 room_id = []
@@ -43,7 +67,7 @@ for i in house_id:
         
 data = {"id":room_id,"number":room_number,"balcony":room_balcony,"floor":room_floor,"houseId":room_houseId,"bedAmount":room_bedAmount}
 rooms = pd.DataFrame(data=data,columns=["id", "number","balcony","floor","houseId","bedAmount"])
-rooms.to_csv("./data/rooms.csv", index=False)
+rooms.to_csv("./data/Room.csv", index=False)
 
 # GENERATE BED
 bed_id = []
@@ -59,13 +83,60 @@ for roomId in room_id:
 
 data = {"id":bed_id,"number":bed_number,"roomId":bed_roomId}
 beds = pd.DataFrame(data=data,columns=["id", "number","roomId"])
-beds.to_csv("./data/beds.csv", index=False)
+beds.to_csv("./data/Bed.csv", index=False)
 
 # GENERATE USERS
 users_login = ["admin", "supervisor"]
 users_password = ["admin","supervisor"]
 data = {"login":users_login,"password":users_password}
 users = pd.DataFrame(data=data,columns=["login", "password"])
-users.to_csv("./data/users.csv", index=False)
+users.to_csv("./data/Users.csv", index=False)
+
+# GENERATE ARRIVAL
+arrival_id = [i for i in range(1,5)]
+arrival_beginningDate = ['2022-07-01','2022-08-01','2023-07-01','2023-08-01']
+arrival_endDate = ['2022-07-15','2022-08-15','2023-07-15','2023-08-15'] 
+arrival_price = [3000, 3000, 4500, 4500]
+data = {"id":arrival_id,"beginningDate":arrival_beginningDate,"endDate":arrival_endDate,"price":arrival_price}
+arrivals = pd.DataFrame(data=data,columns=["id", "beginningDate","endDate","price"])
+arrivals.to_csv("./data/Arrival.csv", index=False)
+
+# GENERATE DETACHMENT
+detachment_id = [i for i in range(1,5)]
+detachment_name = ["Lito2022a","Lito2022b","Lito2023a","Lito2023b"]
+detachment_arrivalId = [i for i in range(1,5)]
+data = {"id":detachment_id,"name":detachment_name,"arrivalId":detachment_arrivalId}
+detachments = pd.DataFrame(data=data,columns=["id", "name","arrivalId"])
+detachments.to_csv("./data/Detachment.csv", index=False)
+
+# GENERATE CONTACT
+contact_id = []
+contact_name = []
+contact_surname = []
+contact_adress = []
+contact_birthDate = []
+contact_phoneNumber = []
+contact_sex = []
+
+ukrainian_male_names = ["Andrii", "Bohdan", "Vitalii", "Yevhen", "Ivan", "Oleh", "Pavlo", "Roman", "Fedir"]
+ukrainian_female_names = ["Halyna", "Daryna", "Zhanna", "Kateryna", "Veronika", "Larysa", "Mariia", "Nataliia", "Svitlana"]
+ukrainian_surnames = ["Antonenko", "Bilenko", "Vasylchenko", "Hrytsenko", "Danylenko", "Yevdokymenko", "Kovalko", "Balanenko", "Ivanchenko", "Kovalenko", "Lysenko", "Melko", "Nesterenko", "Oleksiyenko", "Pavlenko", "Romanchuko", "Sydorenko", "Tkachenko", "Usenko", "Fedorenko"]
+ukrainian_cities = ["Sambir", "Lviv", "Odesa", "Kyiv", "Rivne", "Strii", "Pynyani", "Frankivsk"]
+
+for a in arrival_id:
+    for i in range(1,145):
+        contact_id.append(i)
+        if i%2 == 0:
+            contact_name.append(random.choice(ukrainian_male_names))
+            contact_surname.append(random.choice(ukrainian_surnames))
+            contact_sex.append('m')
+        else:
+            contact_name.append(random.choice(ukrainian_female_names))
+            contact_surname.append(random.choice(ukrainian_surnames))
+            contact_sex.append('f')
+        contact_adress.append(random.choice(ukrainian_cities))
+        contact_birthDate.append(generate_random_kid_date())
+        contact_phoneNumber.append(generate_random_phone_number())
+    
 
 
